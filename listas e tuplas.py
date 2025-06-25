23df64e07fc812dedc39b9b1832e1703ddc5de12
Debora Mendes <debora.tmsilvaa@gmail.com>
2025-06-25 13:18:02 -0300

    Add files via upload

---
+++ listas e tuplas.py
@@ -0,0 +1,21 @@
+numeros = [1,2,3,4,5]
+frutas= ["maça", "banana", "uva"]
+mista = ["debora", 2, "tamirys", 3]
+
+numeros.append(8)
+print(numeros)
+numeros.insert(3, 80)
+print(numeros)
+numeros.remove(1)
+print(numeros)
+ultimo = numeros.pop()
+print(numeros)
+numeros.sort()
+print(numeros)
+numeros.reverse()
+print(numeros)
+print(ultimo)
+
+coordenadas = (10,15,20,36,25,20) #tuplas
+print(coordenadas.count(20))
+print(coordenadas.index(20))
---
+++ quiz.py
@@ -0,0 +1,35 @@
+#quiz
+#Dicionario com perguntas e respostas
+
+quiz={
+    "Qual é o maior planeta do sistema solar?" : "Júpiter",
+    "Quantos continentes existem?" : "Sete",
+    "Quem pintou a Mona Lisa?" : "Leonardo da Vinci",
+    "Qual é o idioma mais falado no mundo?" : "Mandarim",
+    "Quanto é 7 + 5?" : "12",
+    "Qual é a metade de 100?" : "50",
+    "Qual o valor de 9 x 6?" : "54",
+    "Quanto é 81 dividido por 9?" : "9",
+    "Qual planeta é conhecido como planeta vermelho?" : "Marte",
+    "A água ferve a quantos graus Celsius?" : "100°C",
+    "Qual gás respiramos para viver?" : "Oxigênio",
+    "Quantos ossos tem o corpo humano (adulto)?" : "206",
+    "Qual é o estado da água em temperatura ambiente?":"Líquido",
+    "Qual é o plural de pão?": "Pães",
+    "Qual a sílaba tônica da palavra janela?" : "Ne",
+    }
+
+#criar a logica do quiz
+
+acertos = 0
+
+for pergunta, resposta in quiz.items():
+    resposta_usuario = input(pergunta + " ")
+
+    if resposta_usuario.strip().lower() == resposta.lower():
+        print("Resposta Correta")
+        acertos +=1
+    else:
+        print(f"Resposta Errada! A resposta correta é : {resposta}")
+
+print(f"Você Acertou {acertos} de {len(quiz)} perguntas.")
